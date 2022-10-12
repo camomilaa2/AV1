30da2c07ac825092cc144e33c397fbfbb03dc0d5
Camila Soares <camomilaagithub@gmail.com>
2022-10-11 21:24:02 -0300

    AV1

--- src/Questao1.java
+++ src/Questao1.java
@@ -1,15 +1,16 @@
 public class Questao1 {
     public static void main(String[] args) {
         int vetor[] = {3,8,7,9,10,11,80,2,1,50};
+        int j = 0, valor = 0;
         for(int i = 0; i < vetor.length; i++){
-            int valor = vetor[i];
-            int j = i - 1;
-            while( j >= 1 && valor < vetor[j]){
+            valor = vetor[i];
+            j = i - 1;
+            while( (j >= 0) && (valor < vetor[j])){
                 int aux = vetor[j+1];
                 vetor[j+1] = vetor[j];
                 vetor[j] = aux;
-                j--;
-                if(j == 0){
+                j = j - 1;
+                if(j == -1){
                     break;
                 }
             }
